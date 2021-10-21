4e5d7a4a472144c8b62ae2c1b4948ec046d2ae6f
Andrew Weis <adw0059@uah.edu>
2021-10-21 15:51:03 -0500

    feature-django_setup

--- dashboard/cap.py
+++ dashboard/cap.py
@@ -1,16 +1,44 @@
-from cap.CAP import CAP
+from cap_package.CAP import CAP
 from .models import *
 
-run_cap():
+def run_cap():
 	
-	# Run Cap Program 
+	# Run Cap Program #
+
+	masterlist = ""
+
+	cap = CAP(masterlist)
+    cap.ingest_datasets()
+
+    # Run QA
+    cap.run_qa()
+
+    # Execute Climate Tag Check
+    cap.climate_tag_check()
+
+    # Not in Masterlist Check
+    cap.not_in_masterlist_check()
+
+    # Create Metrics
+    cap.create_cdi_metrics()
+    cap.create_warnings_summary()
+
+    # Gather Metrics
+    all_metrics = cap.export_all()
 
 	# and put output in the models/database
 
-	#
+
+	#loop through the retag dictionary:
+
+		#add a record to the retag DB including the data.gov_id and the CAP_id
 
 
 
 
 
 
+
+
+
+run_cap()
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,6 +1,58 @@
 from django.db import models
+from datetime import datetime
 
+class Masterlist(models.Model):
+    cdi_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=500)
+    title = models.CharField(max_length=500)
+    organization = models.CharField(max_length=50, null=True)
+    catalog_url = models.URLField(null=True)
+    api_url = models.URLField(null=True)
+    cdi_themes = models.CharField(max_length=200, null=True)
+    metadata_type = models.CharField(max_length=50, null=True, blank=True)
+    geoplatform_id = models.CharField(max_length=50, null=True)
+    is_active = models.BooleanField(default=True)
+    datagov_ID = models.UUIDField(null=True)
 
+class CAP_Instance(models.Model):
+    cap_id = models.AutoField(primary_key=True)
+    date = models.DateField(default=datetime.date.today())
+    masterlist_count = models.IntegerField()
+    climate_collection_count = models.IntegerField()
+    broken_urls = models.IntegerField()
+    lost_climate_tag = models.IntegerField()
+    not_in_masterlist = models.IntegerField()
+    total_warnings = models.IntegerField()
+
+class Broken_API_Datasets(models.Model):
+    broken_id = models.AutoField(primary_key=True)
+    cap_id = models.IntegerField()
+    datagov_ID = models.UUIDField()
+
+class Retag_Datasets(models.Model):
+    retag_id = models.AutoField(primary_key=True)
+    cap_id = models.IntegerField()
+    datagov_ID = models.UUIDField()
+
+class QA_Updates(models.Model):
+    qa_id = models.AutoField(primary_key=True)
+    cap_id = models.IntegerField()
+    datagov_ID = models.UUIDField()
+    name = models.CharField(max_length=500)
+    title =models.CharField(max_length=500)
+    organization = models.CharField(max_length=50, null=True)
+    catalog_url = models.URLField(null=True)
+    metadata_type = models.CharField(max_length=50, null=True, blank=True)
+
+class Not_in_Masterlist(models.Model):
+    nml_id = models.AutoField(primary_key=True)
+    cap_id = models.IntegerField()
+    title = models.CharField(max_length=500)
+    name = models.CharField(max_length=500)
+    api_url = models.URLField(null=True)
+    catalog_url = models.URLField(null=True)
+
+'''
 # Create your models here.
 class CDI_dataset(models.Model):
     date_id = models.CharField(max_length=20, null=True)
@@ -60,7 +112,7 @@ class warnings_summary(models.Model):
     broken_urls = models.IntegerField()
     lost_climate_tag = models.IntegerField()
     not_in_masterlist = models.IntegerField()
-
+'''
 
 
 
