f9684cc326ce65f457a8899be08d22d9bbc7b7d9
Jenny Wood <jwood@wood-impact.attlocal.net>
2021-10-28 15:08:14 -0500

    Added html files for metrics, warnings, and retag folders

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,13 @@
 from django.urls import include, path
 
 from .views import BaseView
+from . import views
 
 urlpatterns = [
     path("", BaseView.as_view(), name="dashboard_base"),
+    path("warnings", views.warnings, name = "warnings"),
+    path("warnings_instance", views.warnings_instance, name = "warnings_instance"),
+    path("metrics", views.metrics, name = "metrics"),
+    path("retag", views.retag, name = "retag"),
 ] 
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,3 +4,15 @@ from django.views.generic import TemplateView
 # Create your views here.
 class BaseView(TemplateView):
     template_name = "base.html"
+
+def warnings(request):
+    return render(request, "warnings/WARNINGS.html")
+
+def warnings_instance(request):
+    return render(request, "warnings/WARNINGS_INSTANCE.html")
+
+def metrics(request):
+    return render(request, "metrics/METRICS.html")
+
+def retag(request):
+    return render(request, "retag/RETAG.html")
