c9800a8b500e049af73391af93cec16b21dba9d3
Andrew Weis <adw0059@uah.edu>
2021-10-29 11:24:03 -0500

    Setup Main and Warnings View/Controller

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,15 @@
 from django.urls import include, path
 
-from .views import BaseView
+from .views import *
 
 urlpatterns = [
-    path("", BaseView.as_view(), name="dashboard_base"),
+    path("", Main_View.as_view(), name="main"),
+    path("charts", Charts_View.as_view(), name="charts"),
+    path("warnings", Warnings_View.as_view(), name="warnings"),
+    path("warnings-instance", WarningsInstance_View.as_view(), name="warningsinstance"),
+    path("climate-collection", ClimateCollection_View.as_view(), name="climatecollection"),
+    path("cdi-masterlist", Masterlist_View.as_view(), name="masterlist"),
+    path("cdi-masterlist/download", MasterlistDownload_View.as_view(), name="masterlistdownload"),
+    path("cdi-masterlist/qa-updates", QAUpdates_View.as_view(), name="qaupdates"),
 ] 
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,74 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import View
+
+from .models import Masterlist, CAPInstance, BrokenAPI, Retag, QAUpdates, NotInMasterlist
 
 # Create your views here.
 class BaseView(TemplateView):
     template_name = "base.html"
+
+class Main_View(View):
+
+    def get(self, request):
+
+        # CDI Metrics Dictionary
+        all_metrics_qs = CAPInstance.objects.values("date", "masterlist_count", "climate_collection_count")
+        all_metrics = list(all_metrics_qs)
+
+        # Current Status
+        current_metrics_qs = all_metrics_qs.order_by("date").reverse()[:1] # Orders Newest Date First and selects top result
+        current_metrics = list(current_metrics_qs)
+
+        # Total Warnings
+        total_warnings_qs = CAPInstance.objects.values("date", "total_warnings").order_by("date").reverse()
+        total_warnings = list(total_warnings_qs)
+
+        return render(request, "base.html", {'all_metrics':all_metrics, "current_metrics":current_metrics, "total_warnings":total_warnings})
+
+class Charts_View(View):
+
+    def get(self, request):
+
+        return render(request, "base.html")
+
+class Warnings_View(View):
+
+    def get(self, request):
+
+        # All Warnings
+        all_warnings_qa = CAPInstance.objects.values("date", "broken_urls", "lost_climate_tag","not_in_masterlist", "total_warnings")\
+        .order_by("date").reverse()
+        all_warnings = list(all_warnings_qa)
+
+        return render(request, "base.html", {"all_warnings":all_warnings})
+
+class WarningsInstance_View(View):
+
+    def get(self, request):
+
+        return render(request, "base.html")
+
+class ClimateCollection_View(View):
+
+    def get(self, request):
+
+        return render(request, "base.html")
+
+class Masterlist_View(View):
+
+    def get(self, request):
+
+        return render(request, "base.html")
+
+class MasterlistDownload_View(View):
+
+    def get(self, request):
+
+        return render(request, "base.html")
+
+class QAUpdates_View(View):
+
+    def get(self, request):
+
+        return render(request, "base.html")
