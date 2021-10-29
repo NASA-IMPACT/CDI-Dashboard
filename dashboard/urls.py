d866519bf3325853ea7f6781ffa51668b04b735f
unknown <spm0004@uah.edu>
2021-10-29 09:40:16 -0500

    Added masterlist and climate collection html

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path("warnings_instance", views.warnings_instance, name = "warnings_instance"),
     path("metrics", views.metrics, name = "metrics"),
     path("retag", views.retag, name = "retag"),
+    path("qa_updates", views.retag, name = "qa_updates"),
+    path("cdi_masterlist", views.retag, name = "cdi_masterlist"),
+    path("climate_collection", views.retag, name = "climate_collection"),
 ] 
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class BaseView(TemplateView):
-    template_name = "base.html"
+    template_name = "HOMEPAGE.html"
 
 def warnings(request):
     return render(request, "warnings/WARNINGS.html")
@@ -16,3 +16,12 @@ def metrics(request):
 
 def retag(request):
     return render(request, "retag/RETAG.html")
+
+def cdi_masterlist(request):
+    return render(request, "cdi_masterlist/CDI_MASTERLIST.html")
+
+def qa_updates(request):
+    return render(request, "cdi_masterlist/qa_updates/QA_UPDATES.html")
+
+def climate_collection(request):
+    return render(request, "CLIMATE_COLLECTION.html")
