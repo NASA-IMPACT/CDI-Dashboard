0a70dd00f0e2729b2ac041d767b8d09157a8818a
spmcneal <spm0004@uah.edu>
2021-11-04 11:08:50 -0500

    Added content to homepage

--- dashboard/views.py
+++ dashboard/views.py
@@ -18,7 +18,10 @@ class Main_View(View):
 
         # Current Status
         current_metrics_qs = all_metrics_qs.order_by("date").reverse()[:1] # Orders Newest Date First and selects top result
-        current_metrics = list(current_metrics_qs)
+        current_metrics = list(current_metrics_qs)[0]
+        climate_collection_count=current_metrics["climate_collection_count"]
+        masterlist_count=current_metrics["masterlist_count"]
+
 
         # Total Warnings
         total_warnings_qs = CAPInstance.objects.values("date", "total_warnings").order_by("date").reverse()
@@ -29,13 +32,14 @@ class Main_View(View):
         for item in all_metrics:
             dct = {k:[v] for k,v in item.items()}
             timeseries_df=timeseries_df.append(pd.DataFrame.from_dict(dct))
+
         timeseries_df.columns=['Date', "Masterlist", "Climate Collection"]
         fig = px.line(timeseries_df, x='Date', y=timeseries_df.columns,
               hover_data={"Date": "|%B %d, %Y"},
               title='Timeseries')
         graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-        return render(request, "HOMEPAGE.html", {'all_metrics':all_metrics, "current_metrics":current_metrics, "total_warnings":total_warnings, "graphJSON":graphJSON})
+        return render(request, "HOMEPAGE.html", {'all_metrics':all_metrics, "current_metrics":current_metrics, "total_warnings":total_warnings, "graphJSON":graphJSON,"masterlist_count":masterlist_count,"climate_collection_count":climate_collection_count})
 
 class Charts_View(View):
 
