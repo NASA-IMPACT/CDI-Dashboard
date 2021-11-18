a9b972bd6bd16d7b9a4d9dabf9bdbd917f58b8f9
Andrew Weis <adw0059@uah.edu>
2021-11-18 07:05:22 -0600

    Consolidated View Functions

--- dashboard/views.py
+++ dashboard/views.py
@@ -24,11 +24,18 @@ class Main_View(View):
         current_metrics_qs = all_metrics_qs.order_by("date").reverse()[:1] # Orders Newest Date First and selects top result
         current_metrics = list(current_metrics_qs)[0]
         
-
         # Total Warnings
-        total_warnings_qs = CAPInstance.objects.values("cap_id", "date", "total_warnings").order_by("date").reverse()
+        total_warnings_qs = CAPInstance.objects.values("cap_id", "date", "total_warnings").order_by("date").reverse()[:6]
         total_warnings = list(total_warnings_qs)
         
+        graphJSON = self.generate_timeseries_chart(all_metrics)
+        
+        context = {'all_metrics':all_metrics, "current_metrics":current_metrics, "total_warnings":total_warnings, "graphJSON":graphJSON}
+
+        return render(request, "HOMEPAGE.html", context)
+
+    def generate_timeseries_chart(self, all_metrics):
+
         # Generatig Timeseries
         timeseries_df=pd.DataFrame({})
         for item in all_metrics:
@@ -40,10 +47,8 @@ class Main_View(View):
               hover_data={"Date": "|%B %d, %Y"},
               title='Timeseries')
         graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-        
-        context = {'all_metrics':all_metrics, "current_metrics":current_metrics, "total_warnings":total_warnings[:6], "graphJSON":graphJSON}
 
-        return render(request, "HOMEPAGE.html", context)
+        return graphJSON
 
 class Charts_View(View):
 
@@ -72,51 +77,33 @@ class WarningsInstance_View(View):
         cap_instance = CAPInstance.objects.get(cap_id=cap_id)
 
         # Grab Masterlist Atrributes
-        brokenlist = self.get_broken_urls(cap_instance)
-        retaglist = self.get_retag(cap_instance)
+        brokenlist = self.dataset_fetcher(cap_instance, BrokenAPI)
+        retaglist = self.dataset_fetcher(cap_instance, Retag)
         nimlist = self.get_nim(cap_instance)
 
         context = {'date': cap_instance.date, 'brokenlist':brokenlist[:5], 'retaglist': retaglist[:5], 'nimlist': nimlist[:5]}
 
         return render(request, "warnings/WARNINGS_INSTANCE.html", context)
 
-    def get_broken_urls(self, cap_instance):
+    def dataset_fetcher(self, cap_id, model): 
 
-        broken_urls_qs = BrokenAPI.objects.filter(cap_id=cap_instance)
-
-        # Get Masterlist Attributes
-        broken_datasets = []
-
-        for broken in broken_urls_qs:
-            masterlist_obj = broken.datagov_ID
-
-            masterlist_dict = {
-                                'title': masterlist_obj.title,
-                                'catalog_url': masterlist_obj.catalog_url
-            }
-
-            broken_datasets.append(masterlist_dict)
-
-        return broken_datasets
-
-    def get_retag(self, cap_instance):
-
-        retag_qs = Retag.objects.filter(cap_id=cap_instance)
+        queryset = model.objects.filter(cap_id=cap_id)
 
         # Get Masterlist Attributes
-        retag_datasets = []
+        datasets = []
 
-        for retag in retag_qs:
-            masterlist_obj = retag.datagov_ID
+        for item in queryset:
+            masterlist_obj = item.datagov_ID
 
             masterlist_dict = {
                                 'title': masterlist_obj.title,
                                 'catalog_url': masterlist_obj.catalog_url,
             }
 
-            retag_datasets.append(masterlist_dict)
+            datasets.append(masterlist_dict)
 
-        return retag_datasets
+        return datasets
+
 
     def get_nim(self, cap_instance):
 
